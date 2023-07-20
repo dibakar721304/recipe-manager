bef594043794ef405b1bd70597d1fbd9668d1a98
anantdibakar <divakar721304@gmail.com>
2023-07-20 18:46:27 +0200

    Initial project structure changes

---
+++ src/test/java/com/manage/recipe/RecipeManagerServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.manage.recipe;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerServiceApplicationTests {
+
+    //    @Test
+    //    void contextLoads() {}
+}
